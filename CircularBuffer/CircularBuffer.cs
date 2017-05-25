8f0e048c706664be251225f7cef5c1305f94723f
sidd <sidd@WIN-PEJ7T1SAVB5>
2017-05-25 07:16:06 -0400

    Add project files.

---
+++ CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularBuffer
+{
+    public class CircularBuffer
+    {
+        private object[] _buffer;
+        private int _start;
+        private int _end;
+        
+        public CircularBuffer() : this (capacity: 10)
+        {
+
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            _buffer = new object[capacity + 1];
+            _start = 0;
+            _end = 0;
+        }
+
+        public void Write(double value)
+        {
+            _buffer[_end] = value;
+            _end = (_end + 1) % _buffer.Length;
+
+            if (_end == _start)
+            {
+                _start = (_start + 1) % _buffer.Length;
+            }
+        }
+    }
+}
